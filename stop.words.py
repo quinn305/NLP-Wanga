2f11ee24e1de7837a0a7d67b069eddd0c27ab17a
quinn305 <violacat99@gmail.com>
2020-09-08 17:57:09 -0500

    Create stop.words.py

---
+++ stop.words.py
@@ -0,0 +1,34 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+STOP_WORDS = set(
+    """
+    ahambi ata awa ano
+    
+    bulano basi
+    
+    ewe esie
+    
+    isa ino
+    
+    khulwa kho khandi kata khubela khu
+    
+    lano lakini
+    
+    mbu mpaka mana mu
+    
+    na ne ni nende nilwo nio nomba naye nga
+    
+    olwa
+    
+    shichira sana
+    
+    ta taa tawe taawe
+    
+    wa
+    
+    ya yaxaba yee
+    
+    """.split()
+)
